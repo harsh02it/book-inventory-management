ce46acd761972771cbb84f02e03eba6b8edbef5b
harsh02it <harshitsavani@gmail.com>
2024-10-30 00:25:53 -0400

    added database script to the codebase

---
+++ database/insert_records.sql
@@ -0,0 +1,3 @@
+   INSERT INTO Inventory (title, author, genre, publication_date, isbn) VALUES
+   ('The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', '1925-04-10', '9780743273565'),
+   ('1984', 'George Orwell', 'Dystopian', '1949-06-08', '9780451524935');
